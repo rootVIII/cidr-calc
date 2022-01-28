39ac47595cd5797b2f1464b1ffae94281103d1d6
rootVIII <colleyloyejames@gmail.com>
2022-01-28 11:49:30 -0500

    loads as chrome extension

--- background.js
+++ background.js
@@ -1,11 +1,15 @@
 // noinspection JSDeprecatedSymbols
 // eslint-disable-next-line no-undef
-chrome.app.runtime.onLaunched.addListener(() => {
-    // eslint-disable-next-line no-undef
-    chrome.app.window.create('index.html', {
-        outerBounds: {
-            width: 600,
-            height: 270,
-        },
-    });
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === 'complete' && /^http/.test(tab.url)) {
+        // eslint-disable-next-line no-undef
+        chrome.scripting.executeScript({
+            target: { tabId },
+            files: ['./cidr.js'],
+        })
+            .then(() => {
+                console.log('INJECTED THE FOREGROUND SCRIPT.');
+            })
+            .catch((err) => console.log(err));
+    }
 });
